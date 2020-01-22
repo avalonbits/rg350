3564aa1809eb594217518ed8a0573130d91d58a1
Igor Cananea <icc@avalonbits.com>
2020-01-21 23:00:38 -0800

    Commented on build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
+# Remove the existing bouncing ball opk.
 rm -f bouncing_ball.opk && \
+
+# Compile the ball.py to ball.pyc
 python -m compileall ./ && \
-mksquashfs  default.gcw0.desktop ball.pyc icon.png intro_ball.gif bouncing_ball.opk -all-root -no-xattrs -noappend -no-exports
+
+# Create the OPK. It is a squashfs archive so we just list all the files and
+# the last name will be the archive name,
+mksquashfs  default.gcw0.desktop ball.pyc icon.png ball.png bouncing_ball.opk -all-root -no-xattrs -noappend -no-exports
